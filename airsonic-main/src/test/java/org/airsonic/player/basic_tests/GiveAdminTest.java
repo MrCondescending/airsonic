ed5021c3863d226779676d2d285c11968d8e5dc9
MrCondescending <workmand1@nku.edu>
2019-01-30 15:22:15 -0500

    Added basic_tests directory under test directory, as well as added a basic test for Admin rights.

---
+++ airsonic-main/src/test/java/org/airsonic/player/basic_tests/GiveAdminTest.java
@@ -0,0 +1,39 @@
+package org.airsonic.player.basic_tests;
+
+import org.airsonic.player.command.UserSettingsCommand;
+import org.airsonic.player.controller.UserSettingsController;
+import org.airsonic.player.domain.User;
+import static org.junit.Assert.*;
+
+import org.airsonic.player.service.SecurityService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+This is a test of the admin setAdmin feature of the application
+
+Author: Devin Workman (MrCondescending)
+ */
+public class GiveAdminTest {
+    private User testuser = new User("testuser", "test", "test@test.com");
+    private UserSettingsCommand usc = new UserSettingsCommand();
+    @Before
+    public void buildUserAndUSC(){
+
+        usc.setUser(testuser);
+    }
+
+    @After
+    public void removeUser(){
+        this.usc.setDeleteUser(true);
+    }
+
+    @Test
+    public void isAdminWhenGivenAdmin(){
+        testuser.setAdminRole(true);
+        assertTrue(testuser.isAdminRole());
+    }
+
+
+}
