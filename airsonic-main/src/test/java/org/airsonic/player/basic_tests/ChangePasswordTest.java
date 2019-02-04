f94bee3f64733d1a0e44146ba379b20a7281c189
schleperb1 <schleperb1@nku.edu>
2019-02-04 17:00:20 -0500

    Added PasswordChange test

---
+++ airsonic-main/src/test/java/org/airsonic/player/basic_tests/ChangePasswordTest.java
@@ -0,0 +1,38 @@
+package org.airsonic.player.basic_tests;
+import org.airsonic.player.command.UserSettingsCommand;
+import org.airsonic.player.domain.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/*
+This is a test of changing the password of a user.
+
+Author: Brent Schleper
+ */
+public class ChangePasswordTest {
+    private User testuser = new User("username", "pw1", "email@email.com");
+    private UserSettingsCommand usc = new UserSettingsCommand();
+
+    @Before
+    public void buildUserAndUSC(){
+        usc.setUser(testuser);
+    }
+
+
+    @Test
+    public void passwordChangedWhenChangePassword(){
+        testuser.setPassword("MyNewPassword");
+        assertEquals("MyNewPassword",testuser.getPassword());
+    }
+
+    @After
+    public void removeUser(){
+        this.usc.setDeleteUser(true);
+    }
+
+
+}
